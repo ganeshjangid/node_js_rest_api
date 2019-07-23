58f230a6b3e7f78a9f1e1c68f24342297479bb12
Ganesh <webdeveloper.ganesh@gmail.com>
2019-07-23 21:22:15 +0530

    test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # node_js_rest_api
 node_js_rest_api
+
+
+
+
+DB_DIALECT=mysql
+DB_HOST=172.29.67.213
+DB_PORT=3306
+DB_NAME=d_jds
+DB_USER=application
+DB_PASSWORD=s@myD#@mnl@sy
--- server.js
+++ server.js
@@ -39,3 +39,40 @@ if (CONFIG.app === 'dev') {
     // models.sequelize.sync({ force: true });//deletes all tables then recreates them useful for testing and development purposes
 }
 
+// CORS
+app.use(cors());
+
+
+app.use('/v1', v1);
+
+app.use('/', function (req, res) {
+    res.statusCode = 200;//send the appropriate status code
+    res.json({ status: "success", message: "Parcel Pending API", data: {} })
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+
+//This is here to handle all the uncaught promise rejections
+process.on('unhandledRejection', error => {
+    console.error('Uncaught Error', pe(error));
+});
